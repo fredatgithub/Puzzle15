65ff6f08b476610ee6ff76309b93bb747e7e1c3b
Freddy Juhel <domaine75@gmail.com>
2019-04-17 21:47:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Puzzle15
+{
+  static class Program
+  {
+    /// <summary>
+    /// Point d'entrée principal de l'application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      FifteenPuzzle puzzle = new FifteenPuzzle();
+      Form form = puzzle.BuildForm();
+      Application.Run(form);
+    }
+  }
+}
